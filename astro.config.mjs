9f2bacd8e18a152949bf8f89e7ab73223b4cea7c
Pavel Tarakanov <sp28337@yandex.ru>
2026-03-21 11:01:05 +0300

    chore(deps): install mdx v5.0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,28 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, fontProviders } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	site: 'https://plankenart.ru',
-	integrations: [sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.google(),
-			name: "Manrope",
-			cssVariable: "--font-manrope",
-			weights: [300, 400, 500, 600, 700, 800],
-			subsets: ["latin", "cyrillic"],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    site: 'https://plankenart.ru',
+    integrations: [sitemap(), mdx()],
+    fonts: [
+        {
+            provider: fontProviders.google(),
+            name: "Manrope",
+            cssVariable: "--font-manrope",
+            weights: [300, 400, 500, 600, 700, 800],
+            subsets: ["latin", "cyrillic"],
   	},
-		{
-			provider: fontProviders.google(),
-			name: "Inter",
-			cssVariable: "--font-inter",
-			weights: [500, 600, 700, 900],
-			subsets: ["latin", "cyrillic"],
-		}
-	]
+        {
+            provider: fontProviders.google(),
+            name: "Inter",
+            cssVariable: "--font-inter",
+            weights: [500, 600, 700, 900],
+            subsets: ["latin", "cyrillic"],
+        }
+    ]
 });
