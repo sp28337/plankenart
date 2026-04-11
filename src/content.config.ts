bc2380aa6720a393388bd5bf92ebdc7826de725f
Pavel Tarakanov <sp28337@yandex.ru>
2026-04-11 10:35:21 +0300

    feat(oils): add oils collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, reference } from "astro:content";
-import { glob, file } from "astro/loaders";
+import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
 const materials = defineCollection({
@@ -8,22 +8,22 @@ const materials = defineCollection({
     metaTitle:       z.string().max(60),
     metaDescription: z.string().max(160),
 
-    title:       z.string(),
-    name:        z.string(),
-    excerpt:     z.string(),
-    description: z.string(),
-    advantages:  z.array(z.string()),
-    application: z.array(z.string()),
+    title:           z.string(),
+    name:            z.string(),
+    excerpt:         z.string(),
+    description:     z.string(),
+    advantages:      z.array(z.string()),
+    application:     z.array(z.string()),
 
-    relatedObjects: z.array(reference("objects")),
-    materialType: z.string(),
-    woodType: z.string(),
-    tags: z.array(z.string()),
+    relatedObjects:  z.array(reference("objects")),
+    materialType:    z.string(),
+    woodType:        z.string(),
+    tags:            z.array(z.string()),
 
-    sortOrder: z.number().default(99),
-    imageAlt: z.string().optional(),
-    coverImage: image(),
-    heroImage: image(),
+    sortOrder:       z.number().default(99),
+    imageAlt:        z.string().optional(),
+    coverImage:      image(),
+    heroImage:       image(),
     heroImageMobile: image(),
   }),
 });
@@ -31,31 +31,53 @@ const materials = defineCollection({
 const objects = defineCollection({
   loader: glob({ pattern: "**/[^_]*.mdx", base: "./src/content/objects" }),
   schema: ({ image }) => z.object({
-    metaTitle:       z.string().max(60),
-    metaDescription: z.string().max(160),
-    title:       z.string(),
-    excerpt:     z.string(),
-    description: z.string(),
+    metaTitle:         z.string().max(60),
+    metaDescription:   z.string().max(160),
+    title:             z.string(),
+    excerpt:           z.string(),
+    description:       z.string(),
     technicalFeatures: z.array(z.string()).optional(),
-    conclusion: z.string().optional(),
+    conclusion:        z.string().optional(),
     
     tags: z.array(z.string()),
 
-    objectType: z.string(),
-    woodTypes: z.array(z.string()),
+    objectType:    z.string(),
+    woodTypes:     z.array(z.string()),
     materialTypes: z.array(z.string()),
-    materials: z.array(reference("materials")),
+    materials:     z.array(reference("materials")),
+    usedOils:      z.array(z.object({
+      surface:       z.string().optional(), // Название поверхности
+      code:          z.array(z.string()),
+      oil:           reference('oils'),
+    })).optional(),
 
-    sortOrder: z.number().default(99),
-    imageAlt: z.string().optional(),
-    coverImage: image(),
-    heroImage: image(),
+    sortOrder:       z.number().default(99),
+    imageAlt:        z.string().optional(),
+    coverImage:      image(),
+    heroImage:       image(),
     heroImageMobile: image(),
-    images: z.array(image()),
+    images:          z.array(image()),
+  }),
+});
+
+const oils = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.mdx", base: "./src/content/oils" }),
+  schema: ({ image }) => z.object({
+
+    brand:       z.string(),
+    title:       z.string(),
+    titleRu:     z.string(),
+    colors:      z.array(z.object({
+      code:        z.string(),
+      name:        z.string(),
+      image:       image(),
+    })),
+    coverImage:  image(),
   }),
 });
 
 export const collections = { 
   materials,
   objects,
+  oils,
 };
